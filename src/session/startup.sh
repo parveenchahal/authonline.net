f062f961932b1274f3fd1240e2e4cb40e94e775c
Parveen Chahal <mail.chahal@gmail.com>
2020-09-07 00:21:21 +0000

    added Dockerfile and other deployment related

---
+++ src/session/startup.sh
@@ -0,0 +1,15 @@
+echo "Fetching access token for keyvault..."
+identity_url="http://aad-identity-service.default:2424/$AAD_IDENTITY_TENANT?client_id=$AAD_IDENTITY_CLIENTID&secret=$AAD_IDENTITY_SECRET"
+identity_url="$identity_url&resource=https://vault.azure.net"
+accesstoken=$(curl -sS $identity_url | jq -r '.access_token')
+
+secret_name="google-oauth-secret"
+echo "Fetching secret $secret_name from keyvault..."
+data=$(curl -sS "https://pckv1.vault.azure.net/secrets/$secret_name?api-version=2016-10-01" -H "Authorization: Bearer $accesstoken" | jq -r '.value')
+mkdir /etc/secrets
+echo $data | base64 -d > /etc/secrets/google-oauth-secret
+
+export GOOGLE_OAUTH_SECRET_FILE_PATH="/etc/secrets/google-oauth-secret"
+
+export FLASK_APP=session.app.py
+flask run -h 0.0.0.0 -p 5000
---
+++ src/session/update-docker-image.sh
@@ -0,0 +1,12 @@
+docker_hub='pchahal24'
+repo='authonline-session'
+tag=$1
+if [ -z $tag ]
+then
+  tag='latest'
+fi
+image_url='$docker_hub/$repo'
+sudo docker pull $image_url:$tag
+sudo docker ps | grep $repo | awk '{print $1}' | xargs sudo docker stop
+sudo docker rm $repo
+sudo docker run -d -p 5000:5000 --restart=always --name $repo $image_url:$tag
