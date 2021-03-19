da1592574cd7d9db65cf91c9854551fdd5b81ba8
Parveen Chahal <mail.chahal@gmail.com>
2021-03-19 21:46:05 +0000

    removed common folder

--- backend/src/common/http_responses/models/_message_response.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from ... import Model as _Model
-from http import HTTPStatus
-
-@dataclass
-class MessageResponseModel(_Model):
-    message: str
-
--- backend/src/common/http_responses/models/_session_token_response.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-from ... import Model as _Model
-
-@dataclass
-class SessionTokenResponse(_Model):
-    session: str
