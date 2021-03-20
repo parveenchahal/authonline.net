62d28146f73457c22e1c81b4e91ac68ef91bf57e
Parveen Chahal <mail.chahal@gmail.com>
2021-03-20 02:00:56 +0000

    added query params support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Login from './components/login'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+  let args = {q: location.search}
   return (
-    <div>
-      <Login></Login>
-    </div>
-    
+    <Router>
+      <Switch>
+        <Route exact path="/login">
+          <Login {...args}></Login>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
