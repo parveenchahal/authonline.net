88dc44249e7d36fb634ef92fcdebf72337265c7d
Parveen Chahal <mail.chahal@gmail.com>
2021-12-04 18:04:56 +0000

    refactoring

--- backend/src/session/src/app.py
+++ backend/src/session/src/app.py
@@ -39,24 +39,57 @@ logger = logging.getLogger('werkzeug')
 AAD_IDENTITY_TENANT = os.environ['AAD_IDENTITY_TENANT']
 AAD_IDENTITY_CLIENTID = os.environ['AAD_IDENTITY_CLIENTID']
 AAD_IDENTITY_SECRET = os.environ['AAD_IDENTITY_SECRET']
-key_vault_token = AADToken(AAD_IDENTITY_CLIENTID, AAD_IDENTITY_SECRET, 'https://vault.azure.net', tenant=AAD_IDENTITY_TENANT)
+key_vault_token = AADToken(
+    AAD_IDENTITY_CLIENTID,
+    AAD_IDENTITY_SECRET,
+    'https://vault.azure.net',
+    tenant=AAD_IDENTITY_TENANT)
 #===================================================================================
 
 
 #============================== Create Signing Certificate Handler =================
-secret = KeyVaultSecret(config.common.KeyVaultName, config.common.SigningCertificateName, key_vault_token)
-signing_certificate_handler = CertificateFromKeyvault(secret, RedisCache(redis_client, timedelta(hours=1), namespace="public_certificate"))
+secret = KeyVaultSecret(
+    config.common.KeyVaultName,
+    config.common.SigningCertificateName,
+    key_vault_token)
+signing_certificate_handler = CertificateFromKeyvault(
+    secret,
+    RedisCache(redis_client, timedelta(hours=1),
+    namespace="public_certificate"))
 #===================================================================================
 
 
 #============================== Create Storage handlers ============================
-secret = KeyVaultSecret(config.common.KeyVaultName, config.common.CosmosDbConnectionStrings, key_vault_token)
+secret = KeyVaultSecret(
+    config.common.KeyVaultName,
+    config.common.CosmosDbConnectionStrings,
+    key_vault_token)
 client_builder = CosmosClientBuilderFromKeyvaultSecret(secret)
-create_database_container_if_not_exists(client_builder, config.common.DatebaseName, ('user_principal', 'session', 'user_principal_info', 'registration_for_session'), config.common.CosmosOfferThroughput)
-session_storage_container = create_cosmos_container_handler(config.common.DatebaseName, 'session', timedelta(hours=1), client_builder)
-user_storage_container = create_cosmos_container_handler(config.common.DatebaseName, 'user_principal', timedelta(hours=1), client_builder)
-user_info_storage_container = create_cosmos_container_handler(config.common.DatebaseName, 'user_principal_info', timedelta(hours=1), client_builder)
-registration_for_session_storage_container = create_cosmos_container_handler(config.common.DatebaseName, 'registration_for_session', timedelta(hours=1), client_builder)
+create_database_container_if_not_exists(
+    client_builder,
+    config.common.DatebaseName,
+    ('user_principal', 'session', 'user_principal_info', 'registration_for_session'),
+    config.common.CosmosOfferThroughput)
+session_storage_container = create_cosmos_container_handler(
+    config.common.DatebaseName,
+    'session',
+    timedelta(hours=1),
+    client_builder)
+user_storage_container = create_cosmos_container_handler(
+    config.common.DatebaseName,
+    'user_principal',
+    timedelta(hours=1),
+    client_builder)
+user_info_storage_container = create_cosmos_container_handler(
+    config.common.DatebaseName,
+    'user_principal_info',
+    timedelta(hours=1),
+    client_builder)
+registration_for_session_storage_container = create_cosmos_container_handler(
+    config.common.DatebaseName,
+    'registration_for_session',
+    timedelta(hours=1),
+    client_builder)
 
 userinfo_handler = UserInfoHandler(logger, user_info_storage_container)
 user_handler = UserHandler(user_storage_container)
@@ -71,12 +104,17 @@ jwt_handler = JWTHandler(rsa_private_key_handler, rsa_public_key_handler)
 
 
 #============================== Create session handler =============================
-session_handler = SessionHandler(logger, session_storage_container, jwt_handler, refresh_session_interval=config.common.RefreshSessionAfterInterval)
+session_handler = SessionHandler(
+    logger,
+    session_storage_container,
+    jwt_handler,
+    refresh_session_interval=config.common.RefreshSessionAfterInterval)
 #===================================================================================
 
 
 #============================== Create registration handler ========================
-session_registration_handler = SessionRegistrationHandler(registration_for_session_storage_container)
+session_registration_handler = SessionRegistrationHandler(
+    registration_for_session_storage_container)
 #===================================================================================
 
 
@@ -87,29 +125,61 @@ auth_filter.init_session_validator(logger, jwt_handler)
 
 #============================== Register login/logout controllers =========================
 api.add_resource(LoginController, '/login', endpoint="login", resource_class_args=(logger,))
-api.add_resource(LogoutController, '/logout', endpoint="logout", resource_class_args=(logger, session_handler,))
+api.add_resource(
+    LogoutController, '/logout', endpoint="logout", resource_class_args=(logger, session_handler,))
 #===================================================================================
 
 
 #============================== Register Public Certificate controllers =============
-api.add_resource(PublicCertificatesController, '/session/public_certificates', endpoint="session_public_certificates", resource_class_args=(logger, signing_certificate_handler,))
+api.add_resource(
+    PublicCertificatesController,
+    '/session/public_certificates',
+    endpoint="session_public_certificates",
+    resource_class_args=(logger, signing_certificate_handler,))
 #====================================================================================
 
 
 #============================== Register Userinfo controllers =======================
-api.add_resource(UserInfoController, '/userinfo', endpoint="oauth2_userinfo", resource_class_args=(logger, userinfo_handler,))
+api.add_resource(
+    UserInfoController,
+    '/userinfo',
+    endpoint="oauth2_userinfo",
+    resource_class_args=(logger, userinfo_handler,))
 #====================================================================================
 
 
 #============================== Session refresh ====================================
-api.add_resource(RefreshSessionTokenController, '/session/refresh', endpoint="session_refresh", resource_class_args=(logger, session_handler,))
+api.add_resource(
+    RefreshSessionTokenController,
+    '/session/refresh',
+    endpoint="session_refresh",
+    resource_class_args=(logger, session_handler,))
 #====================================================================================
 
 
 #============================== Register GoogleSignIn controllers ===================
 google_oauth = GoogleOauth()
-api.add_resource(GoogleSignInController, '/googlesignin', endpoint="googlesignin", resource_class_args=(logger, google_oauth, session_handler, user_handler, userinfo_handler, session_registration_handler,))
-api.add_resource(GoogleSignInController, '/googlesignin/<type>', endpoint="googlesignin/type", resource_class_args=(logger, google_oauth, session_handler, user_handler, userinfo_handler, session_registration_handler,))
+api.add_resource(
+    GoogleSignInController,
+    '/googlesignin',
+    endpoint="googlesignin",
+    resource_class_args=(
+        logger,
+        google_oauth,
+        session_handler,
+        user_handler,
+        userinfo_handler,
+        session_registration_handler,))
+api.add_resource(
+    GoogleSignInController,'/googlesignin/<type>',
+    endpoint="googlesignin/type",
+    resource_class_args=(
+        logger,
+        google_oauth,
+        session_handler,
+        user_handler,
+        userinfo_handler,
+        session_registration_handler,))
 #====================================================================================
 
 
