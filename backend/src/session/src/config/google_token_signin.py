30c751cb2c7c5e02a1e9120cc75ebb287089614a
Parveen Chahal <mail.chahal@gmail.com>
2022-01-12 20:07:55 +0530

    Update google_token_signin.py

--- backend/src/session/src/config/google_token_signin.py
+++ backend/src/session/src/config/google_token_signin.py
@@ -9,5 +9,5 @@ LoginUrl = None
 def init():
     global LoginUrl, RedirectUri
 
-    RedirectUri = urllib.parse.urljoin(_common.BaseUrl, "googlesignin/auth")
+    RedirectUri = urllib.parse.urljoin(_common.BaseUrl, "session/googlesignin/auth")
     LoginUrl = f"https://accounts.google.com/signin/oauth/identifier?redirect_uri={RedirectUri}&response_type=code&client_id={ClientId}&scope=email%20profile&access_type=offline&prompt=select_account"
